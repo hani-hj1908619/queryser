2cc7e044c1a2a12260efcc008a101a43c51a304b
Hani Jassin <haanijessin@gmail.com>
2023-10-27 17:30:36 +0300

    Update query data structure Change SimpleQueryInfo where_attrs type to allow Equality and Range

--- app/queryser/query.py
+++ app/queryser/query.py
@@ -1,7 +1,8 @@
 import enum
-from typing import Any
+from typing import Any, Union
 from queryser.constants import Table
 import pydantic
+
 
 class FilterClause(pydantic.BaseModel):
     column: str
@@ -10,6 +11,7 @@ class FilterClause(pydantic.BaseModel):
 class EqualityFilter(FilterClause):
     negated: bool = pydantic.Field(False)
     value: Any
+
 
 class RangeFilter(FilterClause):
     min_value: Any = None
@@ -20,17 +22,21 @@ class QueryType(enum.StrEnum):
     NORMAL = enum.auto()
     JOIN = enum.auto()
 
-    
+
 class SimpleQueryInfo(pydantic.BaseModel):
     table: Table | None = None
     res_attrs: list[str] = pydantic.Field(default_factory=list)
-    where_attrs: list[FilterClause] = pydantic.Field(default_factory=list)
-    
+    where_attrs: list[Union[EqualityFilter, RangeFilter]] = pydantic.Field(
+        default_factory=list
+    )
+
+
 class JoinQueryInfo(pydantic.BaseModel):
     table_1_query: SimpleQueryInfo
     table_2_query: SimpleQueryInfo
     table_1_attr: str
     table_2_attr: str
+
 
 class QueryInfo(pydantic.BaseModel):
     type: QueryType = QueryType.NORMAL
