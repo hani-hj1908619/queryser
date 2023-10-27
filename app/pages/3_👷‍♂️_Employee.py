77845f4097ce33d23e544b093d61f1f6b01696d5
Hani Jassin <haanijessin@gmail.com>
2023-10-27 13:59:16 +0300

    Add page config for employee, trade union pages

--- app/pages/3_👷‍♂️_Employee.py
+++ app/pages/3_👷‍♂️_Employee.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import repo
 
-st.set_page_config(layout="wide")
+
+st.set_page_config(
+    page_title="Employee",
+    page_icon="👷‍♂️",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
 st.title("Employee Table")
 st.dataframe(data=repo.read_employee_table())
--- app/pages/4_🏦_Trade_Union.py
+++ app/pages/4_🏦_Trade_Union.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import repo
 
-st.set_page_config(layout="wide")
-st.title("Trade Union")
+st.set_page_config(
+    page_title="Trade Union",
+    page_icon="🏦",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Trade Union Table")
 st.table(repo.read_trade_union_table())
