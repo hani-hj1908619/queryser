2582daa3b4d34ab32bf771d8c0459a0f5130d913
Ahmed Ashraf <root@xps>
2023-11-02 02:18:38 +0300

    push alot

--- app/pages/4_🪙_Cost.py
+++ app/pages/4_🪙_Cost.py
@@ -93,13 +93,47 @@ def join_select_cost(query_info: JoinQueryInfo) -> None:
     st.subheader("Trade Union")
     st.dataframe(df2)
 
-    perms = analyser.get_join_select_costs(df1, df2, query_info)
-    perms.sort(key=lambda perm: sum_costs(perm))
-
-    st.subheader("Query permutations")
-
-    for i, perm in enumerate(perms, start=1):
-        sum = sum_costs(perm)
+    df1_perms, df2_perms, join_cost, final_df = analyser.get_join_select_costs_post(
+        df1, 
+        df2,
+        query_info.table_1_query.where_attrs,
+        query_info.table_2_query.where_attrs,
+    )
+    
+    
+    df1_perms.sort(key=lambda perm: sum_costs(perm))
+    df2_perms.sort(key=lambda perm: sum_costs(perm))
+    
+    st.subheader("Employee filters permutations")
+    for i, perm in enumerate(df1_perms, start=1):
+        sum = sum_costs(perm) or df1.shape[0]
+        with st.expander(f"Perm({i}) - Cost: {math.ceil(sum)}"):
+            if sum == df1.shape[0]:
+                st.write("No filtering")
+            else:
+                st.dataframe(
+                    pd.DataFrame(
+                        {
+                            "Table Size": [clause.cost.initial_size for clause in perm],
+                            "Column": [clause.column for clause in perm],
+                            "Condition": [
+                                analyser.generate_condition_clause(clause)
+                                for clause in perm
+                            ],
+                            "Algorithm": [
+                                clause.cost.algorithm_name for clause in perm
+                            ],
+                            "Matched": [clause.cost.matched_size for clause in perm],
+                            "Equation": [clause.cost.equation for clause in perm],
+                            "Cost": [math.ceil(clause.cost.value) for clause in perm],
+                        }
+                    ),
+                    use_container_width=True,
+                )
+    
+    st.subheader("Trade Union filters permutations")
+    for i, perm in enumerate(df2_perms, start=1):
+        sum = sum_costs(perm) or df2.shape[0]
         with st.expander(f"Perm({i}) - Cost: {math.ceil(sum)}"):
             st.dataframe(
                 pd.DataFrame(
@@ -110,7 +144,9 @@ def join_select_cost(query_info: JoinQueryInfo) -> None:
                             analyser.generate_condition_clause(clause)
                             for clause in perm
                         ],
-                        "Algorithm": [clause.cost.algorithm_name for clause in perm],
+                        "Algorithm": [
+                            clause.cost.algorithm_name for clause in perm
+                        ],
                         "Matched": [clause.cost.matched_size for clause in perm],
                         "Equation": [clause.cost.equation for clause in perm],
                         "Cost": [math.ceil(clause.cost.value) for clause in perm],
@@ -118,6 +154,27 @@ def join_select_cost(query_info: JoinQueryInfo) -> None:
                 ),
                 use_container_width=True,
             )
+    if not perm:
+        st.write("No filtering")
+    
+    st.subheader("Join cost")
+    df1_last_size = df1_perms[-1][-1].cost.matched_size
+    df2_last_size = df2_perms[-1][-1].cost.matched_size
+    st.dataframe(
+        pd.DataFrame(
+            {
+                "Condition": "Employee.trade_union_id = Trade_union.id",
+                "Algorithm": [join_cost.algorithm_name],
+                "Equation": [join_cost.equation],
+                "Tables Size (Before)": f"{df1_last_size} x {df2_last_size}",
+                "Table Size (After)": [final_df.shape[0]],
+                "Cost": [math.ceil(join_cost.value)],
+            }
+        )
+    )
+    
+    st.subheader("Final table")
+    st.dataframe(final_df)
 
 
 main()
--- app/queryser/analyser.py
+++ app/queryser/analyser.py
@@ -7,46 +7,107 @@ from queryser.constants import IndexType, Table
 from queryser.query import EqualityFilter, RangeFilter, Cost, QueryInfo, QueryType
 import repo
 
+def get_filter_cost(
+    df: pd.DataFrame,
+    filterr: EqualityFilter | RangeFilter,
+) -> Cost:
+    if not filterr.table:
+        raise ValueError("Filter table is not set")
+    
+    col_stat = repo.read_column_stats(table=filterr.table, column=filterr.column)
+    
+    if isinstance(filterr, EqualityFilter):
+        
+        if col_stat.column_type == 'number':
+            filterr.value = float(filterr.value)
+        
+        if col_stat.index_type == IndexType.PRIMARY:
+            if filterr.negated:
+                return Cost(
+                    algorithm_name="Linear Search",
+                    equation="n",
+                    value=df.shape[0],
+                )
+            return Cost(
+                algorithm_name="Binary Search",
+                equation="log(n)",
+                value=math.log(df.shape[0], 2) if df.shape[0] else 0,
+            )
+        elif col_stat.index_type == IndexType.NONCLUSTERED:
+            s = df[df[filterr.column] == filterr.value].shape[0]
+            if filterr.negated:
+                return Cost(
+                    algorithm_name="Linear Search",
+                    equation="n",
+                    value=df.shape[0] if df.shape[0] else 0,
+                )
+            elif col_stat.is_unique:
+                return Cost(
+                    algorithm_name="B+ Tree Search",
+                    equation="h + 1",
+                    value=math.log(df.shape[0], 2) + 1 if df.shape[0] else 0,
+                )
+            else:
+                return Cost(
+                    algorithm_name="B+ Tree Search + Sequential Scan",
+                    equation="h + s",
+                    value=math.log(df.shape[0], 2) + s if df.shape[0] else 0,
+                )   
+        else:
+            return Cost(
+                algorithm_name="Linear Search",
+                equation="n",
+                value=df.shape[0],
+            )
+    elif isinstance(filterr, RangeFilter):
+        
+        if col_stat.column_type == 'number':
+            if filterr.min_value is not None:
+                filterr.min_value = float(filterr.min_value)
+            if filterr.max_value is not None:
+                filterr.max_value = float(filterr.max_value)
+        
+        dd = df.copy()
+        if filterr.min_value is not None:
+            dd = dd[dd[filterr.column] >= filterr.min_value]
+        if filterr.max_value is not None:
+            dd = dd[dd[filterr.column] <= filterr.max_value]
+        s = dd.shape[0]
+        
+        if col_stat.index_type == IndexType.PRIMARY:
+            return Cost(
+                algorithm_name="Range Binary Search",
+                equation="log(n) + r",
+                value=math.log(df.shape[0], 2) + s if df.shape[0] else 0,
+            )
+        elif col_stat.index_type == IndexType.NONCLUSTERED:
+            return Cost(
+                algorithm_name="Range B+ Tree Search",
+                equation="h + r",
+                value=math.log(df.shape[0], 2) + s if df.shape[0] else 0,
+            )
+        else:
+            return Cost(
+                algorithm_name="Linear Search",
+                equation="n",
+                value=df.shape[0],
+            )
 
-def primary_key_cost(size: int) -> Cost:
+def get_join_cost(df1: pd.DataFrame, df2: pd.DataFrame, final_df: pd.DataFrame) -> Cost:
+    if df1.shape[0] == 0 or df2.shape[0] == 0:
+        value = 0
+    else:
+        value = (
+            df1.shape[0] *math.log(df1.shape[0], 2)
+            + df1.shape[0]
+            + df2.shape[0] 
+            + final_df.shape[0]
+        )
     return Cost(
-        algorithm_name="Binary Search",
-        equation="log(n)",
-        value=math.log(size, 2) if size else 0,
+        algorithm_name="Sort Merge Join",
+        equation="Br*log2(Br) + Br + Bs + (js * Br * Bs)/Bfr",
+        value=value,
     )
-
-
-def secondary_key_cost(size: int) -> Cost:
-    return Cost(
-        algorithm_name="B+ Tree Search",
-        equation="h + 1",
-        value=math.log(size, 2) + 1 if size else 0,
-    )
-
-
-def primary_key_range_cost(size: int, range_size: int) -> Cost:
-    return Cost(
-        algorithm_name="Range Binary Search",
-        equation="log(n) + r",
-        value=math.log(size, 2) + range_size if size else 0,
-    )
-
-
-def secondary_key_range_cost(size: int, range_size: int) -> Cost:
-    return Cost(
-        algorithm_name="Range B+ Tree Search",
-        equation="h + r",
-        value=math.log(size, 2) + range_size if size else 0,
-    )
-
-
-def non_key_non_indexed_cost(size: int) -> Cost:
-    return Cost(
-        algorithm_name="Linear Search",
-        equation="b/2 (avg)",
-        value=size / 2 if size else 0,
-    )
-
 
 def generate_condition_clause(clause: EqualityFilter | RangeFilter) -> str:
     if isinstance(clause, EqualityFilter):
@@ -79,48 +140,27 @@ def get_simple_select_costs(
         curr_df = df.copy()
         clauses: list[EqualityFilter | RangeFilter] = []
         for clause in perm:
-            col_stat = repo.read_column_stats(table=table, column=clause.column)
+            clause.table = table
             initial_size = curr_df.shape[0]
-
+            cost = get_filter_cost(curr_df, clause)
+            col_type = repo.read_column_stats(table=table, column=clause.column).column_type
             if isinstance(clause, EqualityFilter):
-                if col_stat.index_type == IndexType.PRIMARY:
-                    cost = primary_key_cost(initial_size)
-                elif col_stat.index_type == IndexType.NONCLUSTERED:
-                    cost = secondary_key_cost(initial_size)
-                else:
-                    cost = non_key_non_indexed_cost(initial_size)
-
+                if col_type == 'number':
+                    clause.value = float(clause.value)
                 if clause.negated:
-                    curr_df: pd.DataFrame = curr_df[
-                        curr_df[clause.column] != clause.value
-                    ]
+                    curr_df: pd.DataFrame = curr_df[curr_df[clause.column] != clause.value]
                 else:
-                    curr_df: pd.DataFrame = curr_df[
-                        curr_df[clause.column] == clause.value
-                    ]
-
+                    curr_df: pd.DataFrame = curr_df[curr_df[clause.column] == clause.value]
             elif isinstance(clause, RangeFilter):
-                if clause.min_value is not None and clause.max_value is not None:
+                if col_type == 'number':
+                    if clause.min_value is not None:
+                        clause.min_value = float(clause.min_value)
+                    if clause.max_value is not None:
+                        clause.max_value = float(clause.max_value)
+                if clause.min_value is not None:
                     curr_df = curr_df[curr_df[clause.column] >= clause.min_value]
+                if clause.max_value is not None:
                     curr_df = curr_df[curr_df[clause.column] <= clause.max_value]
-                elif clause.min_value is not None:
-                    curr_df = curr_df[curr_df[clause.column] >= clause.min_value]
-                elif clause.max_value is not None:
-                    curr_df = curr_df[curr_df[clause.column] <= clause.max_value]
-
-                after_size = curr_df.shape[0]
-
-                if col_stat.index_type == IndexType.PRIMARY:
-                    cost = primary_key_range_cost(
-                        size=initial_size, range_size=after_size
-                    )
-                elif col_stat.index_type == IndexType.NONCLUSTERED:
-                    cost = secondary_key_range_cost(
-                        size=initial_size, range_size=after_size
-                    )
-                else:
-                    cost = non_key_non_indexed_cost(initial_size)
-
             else:
                 raise ValueError(f"Invalid clause type {type(clause)}")
 
@@ -135,7 +175,7 @@ def get_simple_select_costs(
     return perms, curr_df
 
 
-def get_join_select_costs(
+def get_join_select_costs_post(
     df1: pd.DataFrame,
     df2: pd.DataFrame,
     df1_filterrs: list[tuple[EqualityFilter | RangeFilter]],
@@ -143,14 +183,23 @@ def get_join_select_costs(
 ) -> tuple[
     list[list[EqualityFilter | RangeFilter]],
     list[list[EqualityFilter | RangeFilter]],
+    Cost,
     pd.DataFrame,
-]:
-    t1_perms, df1_latest = get_simple_select_costs(df1, Table.EMPLOYEE, df1_filterrs)
-    t2_perms, df2_latest = get_simple_select_costs(df2, Table.TRADE_UNION, df2_filterrs)
-
-    final_df = pd.merge(df1_latest, df2_latest, left_on="trade_union_id", right_on="id")
-
-    perms = []
+]: 
+    df1_before, df2_before = df1.copy(), df2.copy()
+    df1_select_cost, df1_after = get_simple_select_costs(df1_before, Table.EMPLOYEE, df1_filterrs)
+    df2_select_cost, df2_after = get_simple_select_costs(df2_before, Table.TRADE_UNION, df2_filterrs)
+    
+    final_df = pd.merge(df1_after, df2_after, left_on="trade_union_id", right_on="id")
+    
+    join_cost = get_join_cost(df1=df1_after, df2=df2_after, final_df=final_df)
+        
+    return (
+        df1_select_cost,
+        df2_select_cost,
+        join_cost,
+        final_df,
+    )
 
 
 def get_best_algorithms(query_info: QueryInfo) -> pd.DataFrame:
