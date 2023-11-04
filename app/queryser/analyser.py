7a0f1ef3c43804b0a317c6f2dcc28152369389e0
Hani Jassin <haanijessin@gmail.com>
2023-11-04 15:07:47 +0300

    Update analyser get_best_algorithms fn Add index_type, is unique columns to best algo table

--- app/queryser/analyser.py
+++ app/queryser/analyser.py
@@ -7,20 +7,20 @@ from queryser.constants import IndexType, Table
 from queryser.query import EqualityFilter, RangeFilter, Cost, QueryInfo, QueryType
 import repo
 
+
 def get_filter_cost(
     df: pd.DataFrame,
     filterr: EqualityFilter | RangeFilter,
 ) -> Cost:
     if not filterr.table:
         raise ValueError("Filter table is not set")
-    
+
     col_stat = repo.read_column_stats(table=filterr.table, column=filterr.column)
-    
+
     if isinstance(filterr, EqualityFilter):
-        
-        if col_stat.column_type == 'number':
+        if col_stat.column_type == "number":
             filterr.value = float(filterr.value)
-        
+
         if col_stat.index_type == IndexType.PRIMARY:
             if filterr.negated:
                 return Cost(
@@ -52,7 +52,7 @@ def get_filter_cost(
                     algorithm_name="B+ Tree Search + Sequential Scan",
                     equation="h + s",
                     value=math.log(df.shape[0], 2) + s if df.shape[0] else 0,
-                )   
+                )
         else:
             return Cost(
                 algorithm_name="Linear Search",
@@ -60,20 +60,19 @@ def get_filter_cost(
                 value=df.shape[0],
             )
     elif isinstance(filterr, RangeFilter):
-        
-        if col_stat.column_type == 'number':
+        if col_stat.column_type == "number":
             if filterr.min_value is not None:
                 filterr.min_value = float(filterr.min_value)
             if filterr.max_value is not None:
                 filterr.max_value = float(filterr.max_value)
-        
+
         dd = df.copy()
         if filterr.min_value is not None:
             dd = dd[dd[filterr.column] >= filterr.min_value]
         if filterr.max_value is not None:
             dd = dd[dd[filterr.column] <= filterr.max_value]
         s = dd.shape[0]
-        
+
         if col_stat.index_type == IndexType.PRIMARY:
             return Cost(
                 algorithm_name="Range Binary Search",
@@ -93,14 +92,15 @@ def get_filter_cost(
                 value=df.shape[0],
             )
 
+
 def get_join_cost(df1: pd.DataFrame, df2: pd.DataFrame, final_df: pd.DataFrame) -> Cost:
     if df1.shape[0] == 0 or df2.shape[0] == 0:
         value = 0
     else:
         value = (
-            df1.shape[0] *math.log(df1.shape[0], 2)
+            df1.shape[0] * math.log(df1.shape[0], 2)
             + df1.shape[0]
-            + df2.shape[0] 
+            + df2.shape[0]
             + final_df.shape[0]
         )
     return Cost(
@@ -108,6 +108,7 @@ def get_join_cost(df1: pd.DataFrame, df2: pd.DataFrame, final_df: pd.DataFrame) 
         equation="Br*log2(Br) + Br + Bs + (js * Br * Bs)/Bfr",
         value=value,
     )
+
 
 def generate_condition_clause(clause: EqualityFilter | RangeFilter) -> str:
     if isinstance(clause, EqualityFilter):
@@ -143,16 +144,22 @@ def get_simple_select_costs(
             clause.table = table
             initial_size = curr_df.shape[0]
             cost = get_filter_cost(curr_df, clause)
-            col_type = repo.read_column_stats(table=table, column=clause.column).column_type
+            col_type = repo.read_column_stats(
+                table=table, column=clause.column
+            ).column_type
             if isinstance(clause, EqualityFilter):
-                if col_type == 'number':
+                if col_type == "number":
                     clause.value = float(clause.value)
                 if clause.negated:
-                    curr_df: pd.DataFrame = curr_df[curr_df[clause.column] != clause.value]
+                    curr_df: pd.DataFrame = curr_df[
+                        curr_df[clause.column] != clause.value
+                    ]
                 else:
-                    curr_df: pd.DataFrame = curr_df[curr_df[clause.column] == clause.value]
+                    curr_df: pd.DataFrame = curr_df[
+                        curr_df[clause.column] == clause.value
+                    ]
             elif isinstance(clause, RangeFilter):
-                if col_type == 'number':
+                if col_type == "number":
                     if clause.min_value is not None:
                         clause.min_value = float(clause.min_value)
                     if clause.max_value is not None:
@@ -185,15 +192,19 @@ def get_join_select_costs_post(
     list[list[EqualityFilter | RangeFilter]],
     Cost,
     pd.DataFrame,
-]: 
+]:
     df1_before, df2_before = df1.copy(), df2.copy()
-    df1_select_cost, df1_after = get_simple_select_costs(df1_before, Table.EMPLOYEE, df1_filterrs)
-    df2_select_cost, df2_after = get_simple_select_costs(df2_before, Table.TRADE_UNION, df2_filterrs)
-    
+    df1_select_cost, df1_after = get_simple_select_costs(
+        df1_before, Table.EMPLOYEE, df1_filterrs
+    )
+    df2_select_cost, df2_after = get_simple_select_costs(
+        df2_before, Table.TRADE_UNION, df2_filterrs
+    )
+
     final_df = pd.merge(df1_after, df2_after, left_on="trade_union_id", right_on="id")
-    
+
     join_cost = get_join_cost(df1=df1_after, df2=df2_after, final_df=final_df)
-        
+
     return (
         df1_select_cost,
         df2_select_cost,
@@ -204,6 +215,8 @@ def get_join_select_costs_post(
 
 def get_best_algorithms(query_info: QueryInfo) -> pd.DataFrame:
     conditions = []
+    index_types = []
+    is_unique = []
     algorithms = []
 
     if query_info.type == QueryType.NORMAL:
@@ -217,6 +230,9 @@ def get_best_algorithms(query_info: QueryInfo) -> pd.DataFrame:
             col_stat = repo.read_column_stats(
                 table=query.table, column=condition.column
             )
+
+            index_types.append(col_stat.index_type or "none")
+            is_unique.append(col_stat.is_unique)
 
             if isinstance(condition, EqualityFilter):
                 if col_stat.index_type == IndexType.PRIMARY:
@@ -235,17 +251,23 @@ def get_best_algorithms(query_info: QueryInfo) -> pd.DataFrame:
 
     elif query_info.type == QueryType.JOIN:
         query = query_info.join
-        
+
         table_queries = [query.table_1_query, query.table_2_query]
         for table_query in table_queries:
             for condition in table_query.where_attrs:
                 table_name = table_query.table
+
                 conditions.append(
                     f"{table_name}.{condition.column} {generate_condition_clause(condition)}"
                 )
+
                 col_stat = repo.read_column_stats(
                     table=table_name, column=condition.column
                 )
+
+                index_types.append(col_stat.index_type or "none")
+                is_unique.append(col_stat.is_unique)
+
                 if isinstance(condition, EqualityFilter):
                     if col_stat.index_type == IndexType.PRIMARY:
                         algorithms.append("Binary Search")
@@ -260,5 +282,12 @@ def get_best_algorithms(query_info: QueryInfo) -> pd.DataFrame:
                         algorithms.append("Range B+ Tree Search")
                     else:
                         algorithms.append("Linear Search")
-                        
-    return pd.DataFrame({"Condition": conditions, "Algorithm": algorithms})
+
+    return pd.DataFrame(
+        {
+            "Condition": conditions,
+            "Index Type": index_types,
+            "Is Unique": is_unique,
+            "Algorithm": algorithms,
+        }
+    )
